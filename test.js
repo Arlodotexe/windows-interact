bf8571784a19306ed491805cd1f5fc0a30703571
Arlodotexe <chuck.fecht@outlook.com>
2018-09-05 13:12:06 -0500

    Add test.js

---
+++ test.js
@@ -0,0 +1,74 @@
+const Win = require('./windows-interact');
+let x = ['write-host "test"', 'write-host "Still alive?"'];
+const supplementals = require('./supplementals');
+
+Win.set.preferences({
+    log: {
+        verbose: {
+            PowerShell: true
+        }
+    }
+});
+
+/* Win.appManager.register({
+    VSCode: {
+		path: Win.path`C:\Program Files\Microsoft VS Code\Code.exe`,
+		onLaunch: function() {
+			Win.log('VSCode launched');
+			setTimeout(() => {
+			}, 500);
+		},
+		onKill: function() {
+			Win.log('VSCode killed');
+
+		}
+	},
+    Pad: {
+        path: Win.path`C:\WINDOWS\system32\notepad.exe`,
+        onLaunch: function() {
+            Win.log('Notepad Launched')
+        },
+        onKill: function() {
+            Win.log('Notepad killed')
+        }
+    },
+})
+
+Win.appManager.register.group({
+    "test": {
+        apps: ["Pad", "VSCode", "boincmgr"],
+        onLaunch: function() {
+            console.log('test launch');
+        }
+    }
+}); */
+
+
+let com = `get-process "notepad" | select ProcessName, MainWindowTitle`;
+
+Win.PowerShell('get-process "notepad" | select ProcessName, MainWindowTitle', result => {
+    console.log('result ', result);
+}, { noLog: true, id: 'test', suppressErrors: false, keepAlive: true });
+
+
+
+// TryforUntil is broken now that every session is pushed to psVars.PowerShellSessions
+
+Win.PowerShell.newCommand(com, result => {
+    console.log(result);
+}, { noLog: true, id: 'test' });
+
+Win.PowerShell('ls', result => {
+    console.log('result 2 ', result);
+}, { noLog: true, id: 'test2', suppressErrors: true, keepAlive: false });
+
+/*
+setTimeout(() => {
+}, 2000);
+/* Win.get.audioDevices.output.isPlaying(result => {
+    console.log(result);
+}); */
+
+/* Win.PowerShell(supplementals.AudioDetection, result => {
+        //console.log(result)
+    }); */
